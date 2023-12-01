6b01649e649724c3d75649ce3b0b923a3fbd7092
afbstorm <aurelien.freyling@bstorm.be>
2023-12-01 10:58:55 +0100

    Update with BehaviorSubject and Observable

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import {Router} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private authStatus = new BehaviorSubject<boolean>(this.hasToken())
   token: string | null = localStorage.getItem('authToken');
   constructor(private _router: Router) { }
 
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+
+  getAuthStatus() {
+    return this.authStatus.asObservable();
+  }
   isAuth () {
-    if (this.token) {
-      // Ici rajoute ta vérif de ton back sur la durée de vie du token
-      return true
-    } else {
-      return false
-    }
-    // Ou return !!this.token
+    return this.authStatus.value;
+  }
+
+  login(token: string) {
+    localStorage.setItem('authToken', token);
+    this.authStatus.next(true);
   }
 
   isNotAuth () {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   username: string = '';
   password: string = '';
   token: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-  constructor (private _router: Router) {}
+  constructor (private _router: Router, private _auth: AuthService) {}
+
+  ngOnInit() {
+    this._auth.getAuthStatus().subscribe(isAuth => {
+      if (isAuth) {
+        this._router.navigate(['/dashboard']);
+      }
+    });
+  }
 
   login(username: string, password: string) {
     if (username === 'test' && password === '1234') {
-      localStorage.setItem('authToken', this.token)
-      this._router.navigate(['/dashboard'])
+      this._auth.login(this.token);
+      this._router.navigate(['/dashboard']);
     }
   }
 }
